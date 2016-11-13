aa373fe6f09066988cd271af531de4009eec3282
Ganesh <asganesh@paypalcorp.com>
2016-11-13 15:33:29 +0530

    Segregated SMS and Email functionalities

--- Server.js
+++ Server.js
@@ -11,13 +11,70 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('C:\\Users\\echinnasamy\\Desktop\\CRT Files\\Donor_App'));
+app.use(express.static('C:\\PP\\Donor'));
 
 app.listen(8889);
 
 var url = 'mongodb://localhost:27017/test';
 
 var db;
+
+
+function sendEmail(subject , message , recepientEmail){
+
+
+  var transporter = nodemailer.createTransport({
+          service: 'Gmail',
+          auth: {
+              user: 'surabitrustee@gmail.com', // Your email id
+              pass: 'bess1234' // Your password
+          }
+      });
+
+
+  var text = 'Hello world  \n\n';
+
+  var mailOptions = {
+      from: 'surabitrustee@gmail.com', // sender address
+      to: recepientEmail, // list of receivers
+      subject: subject, // Subject line
+      text: message //, // plaintext body
+      // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          console.log(error);
+
+      }else{
+          console.log('Message sent: ' + info.response);
+
+      };
+  });
+
+
+
+}
+
+function sendSMS(phoneno,message){
+	var client = twilio('ACf33e0dc0b3526f59f4a3d49b02a5b6d6', '82a54c59c873d25977b267fcd5c1ef8a');
+
+	// Send the text message.
+	client.sendMessage({
+		to: '+919542947251',
+		from: '+19067537957',
+		body: message
+	},function(error, info){
+			if(error){
+					console.log(error);
+
+			}else{
+					console.log('Message sent: ' + info);
+				}
+
+			});
+
+}
 
 MongoClient.connect(url, function(err, mdb) {
 		if(!err) {
@@ -30,20 +87,60 @@ MongoClient.connect(url, function(err, mdb) {
 		}
 });
 
+var CronJob = require('cron').CronJob;
+var job = new CronJob({
+  cronTime: '00 10 12 * * *',
+   onTick: function() {
+
+    console.log('Cron is running ');
+		var currentDate = new Date();
+	  currentDate.setDate(currentDate.getDate() + 7);
+		var timeinmillis = currentDate.getTime();
+
+    var queryJSON = {}
+		queryJSON.dateOfBirth = {};
+		queryJSON.dateOfBirth.gte = timeinmillis;
+
+
+		db.collection('Donors', function(err, collection) {
+	             collection.find(queryJSON).toArray(function(err, items) {
+	                 console.log(items);
+	                 //res.send(items);
+
+									 /*for( var i = 0 ; i < items.length ; i ++){
+
+										 sendSMS(phoneno,message);
+										 sendEmail(subject , message , recepientEmail);
+									 }*/
+
+
+
+	             });
+	         });
+
+					 /**/
+
+
+  },
+  start: false,
+  timeZone: 'America/Los_Angeles'
+});
+job.start();
+
 app.post('/Register_Donor',function(request,response){
 
 	console.log("Called Register_Donor");
-	
-	
+
+
 	var name=request.body.regName;
 	var gender=request.body.regGender;
 	var dateOfBirth=new Date(request.body.regDateOfBirth).getTime();
 	var address=request.body.regAddress;
 	var mobileNumber=request.body.regMobileNumber;
 	var emailID=request.body.regEmailID;
-	
-	
-	
+
+
+
 	db.collection('Donors').insertOne( {
 			"name" : name,
 			"gender" : gender,
@@ -56,7 +153,7 @@ app.post('/Register_Donor',function(request,response){
 		console.log(result);
 			assert.equal(err, null);
 			console.log("Inserted a document into the Donors collection.");
-	});	
+	});
 	response.redirect('/');
 });
 
@@ -64,14 +161,14 @@ app.post('/Add_Donation',function(request,response){
 
 	console.log("Called Add_Donation");
 	console.log(request.body.addName);
-	
+
 	var name=request.body.addName;
 	var mobileNumber=request.body.addMobileNumber;
 	var amount=parseInt(request.body.addAmount);
 	var transferedDate=new Date(request.body.transferedDate).getTime();
 	var donatedDate=new Date(request.body.donatedDate).getTime();
 	var remarks=request.body.addRemarks;
-	
+
 	db.collection('Donations').insertOne( {
 		"name" : name,
 		"mobileNumber" : mobileNumber,
@@ -91,7 +188,7 @@ app.post('/Add_Donation',function(request,response){
 app.post('/fetchDonationReport',function(request, response){
 
 	console.log("fetchDonationReport Called");
-	
+
 	var minValue=request.body.queryMinAmount;
 	var maxValue=request.body.queryMaxAmount;
 	var fromDate=new Date(request.body.queryFromDate).getTime();
@@ -99,9 +196,9 @@ app.post('/fetchDonationReport',function(request, response){
 	var name=request.body.queryDonorName;
 	var mobileNumber=request.body.queryMobileNumber;
 	var cause=request.body.queryCause;
-	
+
 	var queryJSON = {};
-	
+
 	if(minValue!=""){
 		queryJSON.amount={};
 		queryJSON.amount.gte=minValue;
@@ -110,7 +207,7 @@ app.post('/fetchDonationReport',function(request, response){
 		if(maxValue==""||isNaN(maxValue))
 			queryJSON.amount={};
 		queryJSON.amount.lte=maxValue;
-	}	
+	}
 	if(fromDate!=""&&!isNaN(fromDate)){
 		queryJSON.donatedDate={};
 		queryJSON.donatedDate.gte=fromDate;
@@ -126,31 +223,30 @@ app.post('/fetchDonationReport',function(request, response){
 		queryJSON.mobileNumber=mobileNumber;
 	if(cause!="")
 		queryJSON.cause=cause;
-	
+
 	console.log(queryJSON);
-	
+
 	db.collection('Donations', function(err, collection) {
              collection.find(queryJSON).toArray(function(err, items) {
                  console.log(items);
                  //res.send(items);
              });
-         });	
-		
+         });
+
 	response.redirect('/');
 });
-
 
 app.post('/fetchDonorUserReport',function(request, response){
 
 	console.log("fetchDonorUserReport Called");
-	
+
 	var name=request.body.queryDonorName;
 	var mobileNumber=request.body.queryMobileNumber;
 	var cause=request.body.queryCause;
 	var emailID=request.body.queryEmail;
-	
+
 	var queryJSON = {};
-	
+
 	if(name!="")
 		queryJSON.name=name;
 	if(mobileNumber!="")
@@ -159,15 +255,15 @@ app.post('/fetchDonorUserReport',function(request, response){
 		queryJSON.cause=cause;
 	if(emailID!="")
 		queryJSON.emailID=emailID;
-	
+
 	console.log(queryJSON);
-	
+
 	db.collection('Donors', function(err, collection) {
              collection.find(queryJSON).toArray(function(err, items) {
                  console.log(items);
                  //res.send(items);
              });
-         });	
-		
+         });
+
 	response.redirect('/');
 });
